5a8bef12b1ffe7d87ec588d3ab5847bee4fb18fd
ujtk6014 <ujtk6014.m2@keio.jp>
2020-06-01 21:50:32 +0900

    txtファイルへ書き込む関数を追加

--- mission_func/func_test_class.py
+++ mission_func/func_test_class.py
@@ -2,6 +2,7 @@ import time
 import constant as ct
 import led_class
 import sys
+import datetime
 
 class simpleFunc():
     
@@ -15,8 +16,34 @@ class simpleFunc():
         self.flyingTime = 0
         self.goalTime = 0
     
+    def sensor(self): #センサーの値の読み取り
+        #データの読み込み
+        #gps.readgps()など
+        self.writeData()
+        #if not self.state == 2:
+        #    sendRadio()
+    
+    def writeData(self): #txtファイルへの書き込み
+        dt_now = str(datetime.datetime.now()) #現在の日付の取得
+        dt_now = dt_now[0:19] #いらないところを切り取る
+        
+        #ログデータ作成。\マークを入れることで改行してもコードを続けて書くことができる
+        datalog = dt_now + ","\
+                  + str(self.state) + ","\
+                  + str(self.LED_Pre.led_status) + ","\
+                  + str(self.LED_Fly.led_status) + ","\
+                  + str(self.LED_G.led_status)
+        """
+        try: #ファイルが存在しない場合は新規ファイルの作成
+            with open("Test.txt",mode = 'w') as test: #with関数でclose作業を省略
+               test.write(datalog + '\n')
+        except FileExistsError: #ファイルが存在する場合は追記する
+        """
+        with open("test.txt",mode = 'a') as test: # [mode] x:ファイルの新規作成、r:ファイルの読み込み、w:ファイルへの書き込み、a:ファイルへの追記
+            test.write(datalog + '\n')
+          
     def sequence(self):
-        if self.state == 0:#初期化の必要あり
+        if self.state == 0:#初期化の必要あり←initで初期化
             self.preparing()
         elif self.state == 1:
             self.flying()
@@ -26,7 +53,7 @@ class simpleFunc():
             self.finish()
         else:
             self.state = 0
-            
+                
     def preparing(self):
         print('Now it is preparing state')
         if self.preparingTime == 0:
@@ -35,7 +62,7 @@ class simpleFunc():
             self.LED_G.led_off()
             self.preparingTime = time.time()#現在の時刻を取得
             
-        time.sleep(3)
+        time.sleep(1)
         
         if not self.preparingTime == 0:
             if time.time() - self.preparingTime > ct.const.LANDING_TIME_THRE:
@@ -50,7 +77,7 @@ class simpleFunc():
             self.LED_G.led_off()
             self.flyingTime = time.time()#現在の時刻を取得
             
-        time.sleep(3)
+        time.sleep(1)
         
         if not self.flyingTime == 0:
             if time.time() - self.flyingTime > ct.const.LANDING_TIME_THRE:
@@ -65,7 +92,7 @@ class simpleFunc():
             self.LED_G.led_on()
             self.goalTime = time.time()#現在の時刻を取得
             
-        time.sleep(3)
+        time.sleep(1)
         
         if not self.goalTime == 0:
             if time.time() - self.goalTime > ct.const.LANDING_TIME_THRE:
