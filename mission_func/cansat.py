aa0ec111158da89043b542320c9666a45c178171
KazukiOshima <kazuki_28_oshima@keio.jp>
2020-06-12 19:09:12 +0900

    waiting&running

--- mission_func/cansat.py
+++ mission_func/cansat.py
@@ -31,14 +31,17 @@ class Cansat(object):
         self.RED_LED = led.led(ct.const.RED_LED_PIN)
         self.BLUE_LED = led.led(ct.const.BLUE_LED_PIN)
         self.GREEN_LED = led.led(ct.const.GREEN_LED_PIN)
+        self.camera=camera.Camera()
         
         #開始時間の記録
         self.startTime = time.time()
         self.timer = 0
+        self.timestep=0#写真撮影用
         
         #変数
         self.state = 0
         self.laststate = 0
+        self.following=0 # state1の中で、カメラによる検知中か追従中かを区別、どちらもカメラを回しながら行いたいため
         
         #stateに入っている時刻の初期化
         self.preparingTime = 0
@@ -54,6 +57,11 @@ class Cansat(object):
         self.countFlyLoop = 0
         self.countDropLoop = 0
         self.countGoal = 0
+        self.countAreaLoopEnd=0 # 終了判定用
+        self.countAreaLoopStart=0 # 開始判定用
+        self.countAreaLoopLose=0 # 見失い判定用
+        self.countDistanceLoopStart=0 # 距離による開始判定
+        self.countDistanceLoopEnd=0 # 距離による終了判定
         
         #GIOP設定
         GPIO.setmode(GPIO.BCM) #GPIOの設定
@@ -204,11 +212,17 @@ class Cansat(object):
             self.RED_LED.led_off()
             self.BLUE_LED.led_on()
             self.GREEN_LED.led_on()
-        #以下に超音波センサによる動的物体発見プログラム
             
-        if #動的物体の認知反転の完了
-            self.state = 5
-            self.laststate = 5
+        #以下に超音波センサによる動的物体発見プログラム
+        if self.ultrasonic.dist<ct.const.DISTANCE_THRE_START:
+            self.countDistanceLoopStart+=1
+            if self.countDistanceLoopStart>ct.const.COUNT_DISTANCE_LOOP_THRE_START:
+                print("対象認知＆カメラ処理開始")
+                self.state=5
+                self.laststate=5
+                self.countDistanceLoopStart=0
+        else:
+            self.countDistanceLoopStart=0
     
     def running(self):
         if self.runningTime == 0:#時刻を取得してLEDをステートに合わせて光らせる
@@ -218,14 +232,117 @@ class Cansat(object):
             self.GREEN_LED.led_on()
             
         #以下に画像処理走行プログラム
-        
-        if #見失い判定
-            self.state = 4
-            self.laststate = 4
+        self.capture = cv2.VideoCapture(0)
+        while cv2.waitKey(30) < 0:
+            #カメラループの中でセンサの値を取得（記録のために必要？）
+            #self.sensor()
             
-        if #終了判定
-            self.state = 6
-            self.laststate = 6
+            #写真撮影用
+            self.timestep+=1
+            
+            _, frame = self.capture.read() # 動画の読み込み
+            # frame=cv2.resize(frame, (640,480)) # プレビューサイズ（いじらなくてよい）
+            rects = self.camera.find_rect_of_target_color(frame) # 矩形の情報作成
+            
+            # 一定間隔で状況を撮影
+            if self.timestep%200==0:
+                imName=str(self.timestep)+'image.jpg'
+                cv2.imwrite(imName,frame)
+            
+            if len(rects) > 0:
+                rect = max(rects, key=(lambda x: x[2] * x[3]))  # 最大の矩形を探索
+                
+                #各パラメータの計算
+                self.camera.find_center_of_gravity(rect) # 重心の計算
+                self.camera.find_angle(self.camera.cgx) # 角度の計算、絶対値
+                self.camera.find_direction(self.camera.cgx) # 進む方向
+                self.camera.find_area(rect) # 矩形の面積算出
+                
+                #追従開始判定
+                if self.following==0 and self.camera.area>ct.const.AREA_THRE_START:
+                    self.countAreaLoopStart+=1
+                    if self.countAreaLoopStart==1:
+                        self.camera.cgxs=self.camera.cgx
+                        self.camera.cgys=self.camera.cgy
+                    if self.countAreaLoopStart>ct.const.COUNT_AREA_LOOP_THRE_START:
+                        if pow(self.camera.cgx-self.camera.cgxs,2)+pow(self.camera.cgy-self.camera.cgys,2)>ct.const.COG_THRE_START:
+                            print("追従開始")
+                            self.following=1
+                            self.countAreaLoopStart=0
+                else:
+                    self.countAreaLoopStart=0
+                
+                #モーターへの指示を行う
+                if self.following==1:
+                    #print('モーターへの指示')
+                    if self.camera.direct==0:
+                        print('right motor:', 100)
+                        print('left motor:', 100)
+                    if self.camera.direct==1:
+                        print('right motor:', round(100*(1-self.camera.angle/180)))
+                        print('left motor:', 100)
+                    if self.camera.direct==-1:
+                        print('right motor:', 100)
+                        print('left motor:', round(100*(1-self.camera.angle/180)))
+                    #ここにモーターへの指示内容をかく！
+                
+                #見失い判定
+                if self.following==1 and self.camera.area<ct.const.AREA_THRE_LOSE:
+                    self.countAreaLoopLose+=1
+                    if self.countAreaLoopLose>ct.const.COUNT_AREA_LOOP_THRE_LOSE:
+                        self.state=4
+                        self.laststate=4
+                        self.countAreaLoopLose=0
+                        self.following=0
+                        print('見失った！')
+                        self.capture.release()
+                        cv2.destroyAllWindows()
+                        break
+                else:
+                    self.countAreaLoopLose=0
+              
+                #超音波センサを用いた終了判定
+                self.ultrasonic.getDistance()
+                if self.following==1 and self.ultrasonic.dist<ct.const.DISTANCE_THRE_END:
+                    self.countDistanceLoopEnd+=1
+                    if self.countDistanceLoopEnd>ct.const.COUNT_DISTANCE_LOOP_THRE_END:
+                        print("追従終了")
+                        self.state=6
+                        self.lastsate=6
+                        break
+                else:
+                    self.countDistanceLoop=0
+                #矩形の面積を用いた終了判定
+                """
+                if self.area>self.camera.AREA_THRE_END:
+                    self.countAreaLoopEnd+=1
+                    if self.countAreaLoopEnd>self.camera.COUNT_AREA_LOOP_THRE_END:
+                        break
+                else:
+                    self.countAreaLoopEnd=0
+                """
+                
+                cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), (0, 0, 255), thickness=2) # フレームを生成
+                # print (direct)
+                # print (angle)
+                # print (cgx, cgy) #重心座標の出力
+                # print (13500//rect[3]) # 距離の概算出力
+            cv2.drawMarker(frame,(self.camera.cgx,self.camera.cgy),(60,0,0))
+            cv2.imshow('red', frame)
+            
+            #画面に赤い要素が全くない場合の見失い判定
+            if self.following==1 and len(rects)==0:
+                self.countAreaLoopLose+=1
+                if self.countAreaLoopLose>ct.const.COUNT_AREA_LOOP_THRE_LOSE:
+                    self.state=4
+                    self.laststate=4
+                    self.countAreaLoopLose=0
+                    self.following=0
+                    print('見失った2！')
+                    self.capture.release()
+                    cv2.destroyAllWindows()
+                    break
+
     
     def goal(self):
         if self.goalTime == 0:#時刻を取得してLEDをステートに合わせて光らせる
@@ -241,6 +358,10 @@ class Cansat(object):
             self.rightmotor.stop()
             self.leftmotor.stop()
         self.countGoal+ = 1
+        
+        self.capture.release()
+        cv2.destroyAllWindows()
+        sys.exit()
             
 if __name__ == "__main__":
     pass
