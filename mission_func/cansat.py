a289f40b0710d8244eda9d5fae32032ab3934581
KazukiOshima <kazuki_28_oshima@keio.jp>
2020-06-16 22:20:51 +0900

    loop1version

--- mission_func/cansat.py
+++ mission_func/cansat.py
@@ -233,113 +233,105 @@ class Cansat(object):
             self.GREEN_LED.led_on()
             
         #以下に画像処理走行プログラム
-        self.capture = cv2.VideoCapture(0)
-        while cv2.waitKey(30) < 0:
-            #カメラループの中でセンサの値を取得（記録のために必要？）
-            #self.sensor()
+        
+        #写真撮影用
+        self.timestep+=1
+        
+        _, frame = self.capture.read() # 動画の読み込み
+        # frame=cv2.resize(frame, (640,480)) # プレビューサイズ（いじらなくてよい）
+        rects = self.camera.find_rect_of_target_color(frame) # 矩形の情報作成
+        
+        # 一定間隔で状況を撮影
+        if self.timestep%200==0:
+            imName=str(self.timestep)+'image.jpg'
+            cv2.imwrite(imName,frame)
+        
+        if len(rects) > 0:
+            rect = max(rects, key=(lambda x: x[2] * x[3]))  # 最大の矩形を探索
             
-            #写真撮影用
-            self.timestep+=1
+            #各パラメータの計算
+            self.camera.find_center_of_gravity(rect) # 重心の計算
+            self.camera.find_angle(self.camera.cgx) # 角度の計算、絶対値
+            self.camera.find_direction(self.camera.cgx) # 進む方向
+            self.camera.find_area(rect) # 矩形の面積算出
             
-            _, frame = self.capture.read() # 動画の読み込み
-            # frame=cv2.resize(frame, (640,480)) # プレビューサイズ（いじらなくてよい）
-            rects = self.camera.find_rect_of_target_color(frame) # 矩形の情報作成
+            #追従開始判定
+            if self.following==0 and self.camera.area>ct.const.AREA_THRE_START:
+                self.countAreaLoopStart+=1
+                if self.countAreaLoopStart==1:
+                    self.camera.cgxs=self.camera.cgx
+                    self.camera.cgys=self.camera.cgy
+                if self.countAreaLoopStart>ct.const.COUNT_AREA_LOOP_THRE_START:
+                    if pow(self.camera.cgx-self.camera.cgxs,2)+pow(self.camera.cgy-self.camera.cgys,2)>ct.const.COG_THRE_START:
+                        print("追従開始")
+                        self.following=1
+                        self.countAreaLoopStart=0
+            else:
+                self.countAreaLoopStart=0
             
-            # 一定間隔で状況を撮影
-            if self.timestep%200==0:
-                imName=str(self.timestep)+'image.jpg'
-                cv2.imwrite(imName,frame)
+            #モーターへの指示を行う
+            if self.following==1:
+                #print('モーターへの指示')
+                if self.camera.direct==0:
+                    print('right motor:', 100)
+                    print('left motor:', 100)
+                if self.camera.direct==1:
+                    print('right motor:', round(100*(1-self.camera.angle/180)))
+                    print('left motor:', 100)
+                if self.camera.direct==-1:
+                    print('right motor:', 100)
+                    print('left motor:', round(100*(1-self.camera.angle/180)))
+                #ここにモーターへの指示内容をかく！
             
-            if len(rects) > 0:
-                rect = max(rects, key=(lambda x: x[2] * x[3]))  # 最大の矩形を探索
-                
-                #各パラメータの計算
-                self.camera.find_center_of_gravity(rect) # 重心の計算
-                self.camera.find_angle(self.camera.cgx) # 角度の計算、絶対値
-                self.camera.find_direction(self.camera.cgx) # 進む方向
-                self.camera.find_area(rect) # 矩形の面積算出
-                
-                #追従開始判定
-                if self.following==0 and self.camera.area>ct.const.AREA_THRE_START:
-                    self.countAreaLoopStart+=1
-                    if self.countAreaLoopStart==1:
-                        self.camera.cgxs=self.camera.cgx
-                        self.camera.cgys=self.camera.cgy
-                    if self.countAreaLoopStart>ct.const.COUNT_AREA_LOOP_THRE_START:
-                        if pow(self.camera.cgx-self.camera.cgxs,2)+pow(self.camera.cgy-self.camera.cgys,2)>ct.const.COG_THRE_START:
-                            print("追従開始")
-                            self.following=1
-                            self.countAreaLoopStart=0
-                else:
-                    self.countAreaLoopStart=0
-                
-                #モーターへの指示を行う
-                if self.following==1:
-                    #print('モーターへの指示')
-                    if self.camera.direct==0:
-                        print('right motor:', 100)
-                        print('left motor:', 100)
-                    if self.camera.direct==1:
-                        print('right motor:', round(100*(1-self.camera.angle/180)))
-                        print('left motor:', 100)
-                    if self.camera.direct==-1:
-                        print('right motor:', 100)
-                        print('left motor:', round(100*(1-self.camera.angle/180)))
-                    #ここにモーターへの指示内容をかく！
-                
-                #見失い判定
-                if self.following==1 and self.camera.area<ct.const.AREA_THRE_LOSE:
-                    self.countAreaLoopLose+=1
-                    if self.countAreaLoopLose>ct.const.COUNT_AREA_LOOP_THRE_LOSE:
-                        self.state=4
-                        self.laststate=4
-                        self.countAreaLoopLose=0
-                        self.following=0
-                        print('見失った！')
-                        self.capture.release()
-                        cv2.destroyAllWindows()
-                        break
-                else:
-                    self.countAreaLoopLose=0
-              
-                #超音波センサを用いた終了判定
-                self.ultrasonic.getDistance()
-                if self.following==1 and self.ultrasonic.dist<ct.const.DISTANCE_THRE_END:
-                    self.countDistanceLoopEnd+=1
-                    if self.countDistanceLoopEnd>ct.const.COUNT_DISTANCE_LOOP_THRE_END:
-                        print("追従終了")
-                        cv2.imwrite('finish.jpg',frame)
-                        self.state=6
-                        self.lastsate=6
-                        break
-                else:
-                    self.countDistanceLoopEnd=0
-                #矩形の面積を用いた終了判定
-                """
-                if self.camera.area>ct.const.AREA_THRE_END:
-                    self.countAreaLoopEnd+=1
-                    if self.countAreaLoopEnd>ct.const.COUNT_AREA_LOOP_THRE_END:
-                        break
-                else:
-                    self.countAreaLoopEnd=0
-                """
-                
-                cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), (0, 0, 255), thickness=2) # フレームを生成
-            cv2.drawMarker(frame,(self.camera.cgx,self.camera.cgy),(60,0,0))
-            cv2.imshow('red', frame)
-            
-            #画面に赤い要素が全くない場合の見失い判定
-            if self.following==1 and len(rects)==0:
+            #見失い判定
+            if self.following==1 and self.camera.area<ct.const.AREA_THRE_LOSE:
                 self.countAreaLoopLose+=1
                 if self.countAreaLoopLose>ct.const.COUNT_AREA_LOOP_THRE_LOSE:
                     self.state=4
                     self.laststate=4
                     self.countAreaLoopLose=0
                     self.following=0
-                    print('見失った2！')
-                    self.capture.release()
+                    print('見失った！')
                     cv2.destroyAllWindows()
+            else:
+                self.countAreaLoopLose=0
+          
+            #超音波センサを用いた終了判定
+            self.ultrasonic.getDistance()
+            if self.following==1 and self.ultrasonic.dist<ct.const.DISTANCE_THRE_END:
+                self.countDistanceLoopEnd+=1
+                if self.countDistanceLoopEnd>ct.const.COUNT_DISTANCE_LOOP_THRE_END:
+                    print("追従終了")
+                    cv2.imwrite('finish.jpg',frame)
+                    self.state=6
+                    self.lastsate=6
+            else:
+                self.countDistanceLoopEnd=0
+            #矩形の面積を用いた終了判定
+            """
+            if self.camera.area>ct.const.AREA_THRE_END:
+                self.countAreaLoopEnd+=1
+                if self.countAreaLoopEnd>ct.const.COUNT_AREA_LOOP_THRE_END:
                     break
+            else:
+                self.countAreaLoopEnd=0
+            """
+            
+            cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), (0, 0, 255), thickness=2) # フレームを生成
+        cv2.drawMarker(frame,(self.camera.cgx,self.camera.cgy),(60,0,0))
+        cv2.imshow('red', frame)
+        cv2.waitKey(1)
+        
+        #画面に赤い要素が全くない場合の見失い判定
+        if self.following==1 and len(rects)==0:
+            self.countAreaLoopLose+=1
+            if self.countAreaLoopLose>ct.const.COUNT_AREA_LOOP_THRE_LOSE:
+                self.state=4
+                self.laststate=4
+                self.countAreaLoopLose=0
+                self.following=0
+                print('見失った2！')
+                cv2.destroyAllWindows()
 
     
     def goal(self):
