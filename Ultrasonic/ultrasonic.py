188ff64f5239cf5de5cca0c62d0dc4a106a605fc
ujtk6014 <ujtk6014.m2@keio.jp>
2020-07-01 13:04:51 +0900

    整理しました

--- Ultrasonic/ultrasonic.py
+++ Ultrasonic/ultrasonic.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import time
+import constant as ct
 
 class Ultrasonic(object):
-    
+  
     def __init__(self):
         self.dist=0.0
     
     # 距離を読む関数
     def getDistance(self):
+        
         # 必要なライブラリのインポート・設定
         import RPi.GPIO as GPIO
 
         # 使用するピンの設定
         GPIO.setmode(GPIO.BCM)
-        TRIG = 24 # GPIO02(Pin3)
-        ECHO = 23 # GPIO03(Pin5)
 
         # ピンのモードをそれぞれ出力用と入力用に設定
-        GPIO.setup(TRIG,GPIO.OUT)
-        GPIO.setup(ECHO,GPIO.IN)
-        GPIO.output(TRIG, GPIO.LOW)
-
+        GPIO.setup(ct.const.ULTRASONIC_TRIG,GPIO.OUT)
+        GPIO.setup(ct.const.ULTRASONIC_ECHO,GPIO.IN)
+        GPIO.output(ct.const.ULTRASONIC_TRIG, GPIO.LOW)
+        
         # TRIG に短いパルスを送る
-        GPIO.output(TRIG, GPIO.HIGH)
+        GPIO.output(ct.const.ULTRASONIC_TRIG, GPIO.HIGH)
         time.sleep(0.00001)
-        GPIO.output(TRIG, GPIO.LOW)
+        GPIO.output(ct.const.ULTRASONIC_TRIG, GPIO.LOW)
 
         # ECHO ピンがHIGHになるのを待つ
         signaloff = time.time()
-        while GPIO.input(ECHO) == GPIO.LOW:
+        while GPIO.input(ct.const.ULTRASONIC_ECHO) == GPIO.LOW:
             signaloff = time.time()
 
         # ECHO ピンがLOWになるのを待つ
         signalon = signaloff
         while time.time() < signaloff + 0.1:
-            if GPIO.input(ECHO) == GPIO.LOW:
+            if GPIO.input(ct.const.ULTRASONIC_ECHO) == GPIO.LOW:
                 signalon = time.time()
                 break
 
--- integ_test/test.py
+++ integ_test/test.py
@@ -5,30 +5,84 @@
 
 import gps
 import ultrasonic
-import BNO055_new
+import bno055
 import time
+import camera
+import cv2
+import sys
+import constant as ct
 
-TRIGPIN=23 #定数
-ECHOPIN=18 #定数
 
-try:
-    GPS=gps.GPS()
-    GPS.setupGPS()
-    ultrasonic.setupUltrasonic(TRIGPIN,ECHOPIN)
-    bno=BNO055_new.BNO055()
-    if bno.begin() is not True:
-        print("Error initializing device")
-        exit()
-    #time.sleep(1)
-    bno.setExternalCrystalUse(True) 
-    while True:
-        GPS.gpsread()
-        distance=ultrasonic.getDistance()
-        print(bno.getVector(BNO055_new.BNO055.VECTOR_GYROSCOPE))
-        #BNO055_new.testbno()
-        time.sleep(1)
+class Test(object):
+    
+    def __init__(self):
+        #オブジェクトの生成
+        self.gps = gps.GPS()
+        self.bno055 = bno055.BNO()
+        self.radio = radio.radio()
+        ultrasonic.setupUltrasonic(ct.const.ULTRASONIC_TRIG,ct.const.ULTRASONIC_ECHO)
+        self.camera=camera.Camera()
+        
+    def setup(self):
+        self.gps.setupGps()
+        if self.bno.begin() is not True:
+            print("Error initializing device")
+            exit()
+        self.bno.setExternalCrystalUse(True)
+        
+        
+    
+    def sensorWrite(self):
+        self.gps.gpsread()
+        self.bno055.bnoread()
+        ultrasonic.getDistance()
+        datalog = str(self.timer) + ","\
+                  + str(self.gps.Time) + ","\
+                  + str(self.gps.Lat) + ","\
+                  + str(self.gps.Lon) + ","\
+                  + str(self.bno055.gx) + ","\
+                  + str(self.bno055.gy) + ","\
+                  + str(self.bno055.gz) + ","\
+                  + str(self.bno055.Ax) + ","\
+                  + str(self.bno055.Ay) + ","\
+                  + str(self.bno055.Az) + ","\
+                  + str(self.ultrasonic.distance)
+        print(datalog)
+    
+    def camera(self):
+        #以下に画像処理走行プログラム
+            _, frame = self.capture.read() # 動画の読み込み
+            # frame=cv2.resize(frame, (640,480)) # プレビューサイズ（いじらなくてよい）
+            rects = self.camera.find_rect_of_target_color(frame) # 矩形の情報作成
+            
+            if len(rects) > 0:
+                rect = max(rects, key=(lambda x: x[2] * x[3]))  # 最大の矩形を探索
+                
+                #各パラメータの計算
+                self.camera.find_center_of_gravity(rect) # 重心の計算
+           
+                cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), (0, 0, 255), thickness=2) # フレームを生成
+            cv2.drawMarker(frame,(self.camera.cgx,self.camera.cgy),(60,0,0))
+            cv2.imshow('red', frame)
+            cv2.waitKey(1)
 
-except KeyboardInterrupt:        #例外（CTRL+Cが押された時)処理
-    ultrasonic_class.end()                       #endを実行
+if __name__ == "__main__":
+    test=Test()
+    test.setup()
+    self.capture = cv2.VideoCapture(0)
+    try:
+        while True:
+            test.sensorWrite()
+            sleep(0.01)
+            test.camera()
+            sleep(0.01)
+    except KeyboardInterrupt:
+        print('finished')
+        GPIO.cleanup()
+        pass
+
+
+   
+
 
 
